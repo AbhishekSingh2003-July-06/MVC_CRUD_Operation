f153d76acb156fd4455fd730854735717aa095fe
Abhishek kumar Singh <142382434+AbhishekSingh2003-July-06@users.noreply.github.com>
2024-08-09 12:18:50 +0530

    Add files via upload

---
+++ Class1Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationRevisionCRUD.Models
+{
+    public class Class1Model
+    {
+        public int id { get; set; }
+        [Required (ErrorMessage ="Pls enter ur name")]
+
+        public string name { get; set; }
+        [Required]
+        public string email_id { get; set; }
+        [Required]
+        public string contact_No { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public decimal? salary { get; set; }
+     
+    }
+}
